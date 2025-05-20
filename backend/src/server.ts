c1094a835355b1fdf4c093faf9b0f725904e7c9d
Anshor J <anshorje@gmail.com>
2025-05-20 21:01:53 +0700

    Refactor config and add rate limiter

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -3,7 +3,6 @@ import helmet from "helmet";
 import cors from "cors";
 import compression from "compression";
 import morgan from "morgan";
-import configApp from "./config/app";
 import { errorHandler, notFoundHandler } from "./utils/error-handler";
 import rateLimit from "express-rate-limit";
 import config from "~/config";
@@ -13,15 +12,15 @@ import permissionRouter from "./routers/permission";
 const app = express();
 
 app.use(helmet());
-// app.use(
-//   rateLimit({
-//     windowMs: 1 * 60 * 1000, // 1 minute
-//     max: 100, // limit each IP to 100 requests per window
-//   }),
-// );
+app.use(
+  rateLimit({
+    windowMs: 1 * 60 * 1000, // 1 minute
+    max: 100, // limit each IP to 100 requests per window
+  }),
+);
 app.use(
   cors({
-    origin: config.app.frontendUrl,
+    origin: config.frontendUrl,
     methods: ["GET", "POST", "PUT", "PATCH", "DELETE"],
   }),
 );
@@ -30,7 +29,7 @@ app.use(express.json());
 
 app.use(morgan("dev"));
 
-// app.use("/api/auth", authRouter);
+app.use("/api/auth", authRouter);
 // app.use("/api/permissions", permissionRouter);
 
 app.get("/", (req, res) => {
@@ -42,6 +41,6 @@ app.get("/", (req, res) => {
 app.use(notFoundHandler);
 app.use(errorHandler);
 
-app.listen(configApp.port, () => {
-  console.log(`Server running on http://localhost:${configApp.port}`);
+app.listen(config.port, () => {
+  console.log(`Server running on http://localhost:${config.port}`);
 });
