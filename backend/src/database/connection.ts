8dd87549ec8fa1604b6a4f4d39858aa102bc869d
Anshor J <anshorje@gmail.com>
2025-05-20 21:01:04 +0700

    Update database connection

--- backend/src/database/connection.ts
+++ backend/src/database/connection.ts
@@ -1,12 +1,7 @@
 import { default as baseKnex } from "knex";
 // @ts-ignore
 import knexConfig from "../../knexfile";
-import config from "~/config";
 
-const knex = baseKnex(
-  config.app.nodeEnv === "production"
-    ? knexConfig.production
-    : knexConfig.development,
-);
+const knex = baseKnex(knexConfig);
 
 export default knex;
