6b24f1779dd34a29064b6ebf4ff9539bd2a003a6
Anshor J <anshorje@gmail.com>
2025-05-20 21:02:11 +0700

    Convert to typescript

---
+++ backend/src/migrations/20250129040838_create_users_table.ts
@@ -0,0 +1,18 @@
+import { Knex } from "knex";
+import config from "../config";
+
+
+export async function up(knex: Knex) {
+  return knex.schema.createTable('users', (table) => {
+    table.increments("id").primary();
+    table.string("name", 255).notNullable();
+    table.string("email", 150).notNullable().unique().index();
+    table.string("password", 255).notNullable();
+    table.boolean("is_active").defaultTo(true);
+    table.timestamps(true, true);
+  });
+}
+
+export async function down(knex: Knex) {
+  return knex.schema.dropTable('users');
+}
---
+++ backend/src/migrations/20250129042226_create_password_resets_table.ts
@@ -0,0 +1,17 @@
+import { Knex } from "knex";
+import config from "../config";
+
+export async function up(knex: Knex) {
+  return knex.schema.createTable("password_resets", (table) => {
+    table.increments("id").primary();
+    table.integer("user_id").notNullable();
+    table.string("email").notNullable();
+    table.string("token").notNullable();
+    table.timestamp("expires_at").notNullable();
+    table.timestamps(true, true);
+  });
+}
+
+export async function down(knex: Knex) {
+  return knex.schema.dropTable("password_resets");
+}
