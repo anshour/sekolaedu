bb0d9869a2910ec73ea59eade641ae96ece15b0e
Anshor J <anshorje@gmail.com>
2025-05-24 10:41:20 +0700

    Remove all frontend

--- frontend/app/routes.ts
+++
@@ -1,4 +0,0 @@
-import { type RouteConfig, route } from "@react-router/dev/routes";
-import { flatRoutes } from "@react-router/fs-routes";
-
-export default [...(await flatRoutes())] satisfies RouteConfig;
