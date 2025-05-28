59de80cbd3207cf4c37552c2537eb240d3c7dc8a
Anshor J <anshorje@gmail.com>
2025-05-28 22:12:18 +0700

    Add refetch user method

--- frontend/src/context/use-user.ts
+++ frontend/src/context/use-user.ts
@@ -1,3 +1,4 @@
+import http from "@/utils/http";
 import { create } from "zustand";
 import { persist } from "zustand/middleware";
 
@@ -11,6 +12,7 @@ interface UserStore {
     photo: string;
     permissions?: string[];
   };
+  refetchUser: () => Promise<void>;
   clearUser: () => void;
   isAuthenticated: () => boolean;
   isHydrated: boolean;
@@ -25,6 +27,18 @@ const useUser = create<UserStore>()(
       isAuthenticated: () => get().user !== null,
       isHydrated: false,
       onHydrated: () => set({ isHydrated: true }),
+      refetchUser: async () => {
+        try {
+          const userData = await http
+            .get("/auth/me")
+            .then((res) => res.data?.user);
+
+          set({ user: userData });
+        } catch (error: any) {
+          console.error(error);
+          set({ user: null });
+        }
+      },
     }),
     {
       name: "user-storage",
