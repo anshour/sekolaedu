e248c112695bd15fb82f1af91d5cc7288ffa06fe
Anshor J <anshorje@gmail.com>
2025-05-28 22:17:01 +0700

    Redirect after login based on role

--- frontend/src/pages/home.tsx
+++ frontend/src/pages/home.tsx
@@ -1,95 +1,31 @@
-import UserLayout from "@/components/layout/user-layout";
-import { Box, Card, } from "@chakra-ui/react";
+import useUser from "@/context/use-user";
+import { useRouter } from "next/router";
+import { useEffect } from "react";
 
 export default function Home() {
-  return (
-    <>
-      <UserLayout>
-        <Card.Root>
-          <Card.Body>
-            <br />
-            a
-            <br />
-          </Card.Body>
-        </Card.Root>
-        <Card.Root>
-          <Card.Body>
-            <br />
-            a
-            <br />
-          </Card.Body>
-        </Card.Root>
-        <Card.Root>
-          <Card.Body>
-            <br />
-            a
-            <br />
-          </Card.Body>
-        </Card.Root>
-        <Card.Root>
-          <Card.Body>
-            <br />
-            a
-            <br />
-          </Card.Body>
-        </Card.Root>
-        <Card.Root>
-          <Card.Body>
-            <br />
-            a
-            <br />
-          </Card.Body>
-        </Card.Root>
-        <Card.Root>
-          <Card.Body>
-            <br />
-            a
-            <br />
-          </Card.Body>
-        </Card.Root>
-        <Card.Root>
-          <Card.Body>
-            <br />
-            a
-            <br />
-          </Card.Body>
-        </Card.Root>
-        <Card.Root>
-          <Card.Body>
-            <br />
-            a
-            <br />
-          </Card.Body>
-        </Card.Root>
-        <Card.Root>
-          <Card.Body>
-            <br />
-            a
-            <br />
-          </Card.Body>
-        </Card.Root>
-        <Card.Root>
-          <Card.Body>
-            <br />
-            a
-            <br />
-          </Card.Body>
-        </Card.Root>
-        <Card.Root>
-          <Card.Body>
-            <br />
-            a
-            <br />
-          </Card.Body>
-        </Card.Root>
-        <Card.Root>
-          <Card.Body>
-            <br />
-            a
-            <br />
-          </Card.Body>
-        </Card.Root>
-      </UserLayout>
-    </>
-  );
+  const router = useRouter();
+  const user = useUser((state) => state.user);
+
+  useEffect(() => {
+    if (!user) {
+      router.push("/auth/login");
+      return;
+    }
+
+    if (user && user.role_name === "admin") {
+      router.push("/dashboard/admin");
+      return;
+    }
+
+    if (user && user.role_name === "student") {
+      router.push("/dashboard/student");
+      return;
+    }
+
+    router.push(
+      `/error?message=Unauthorized access for role ${user.role_name}.code_r1&previous_path=/home`
+    );
+  }, [user]);
+
+  return <></>;
 }
