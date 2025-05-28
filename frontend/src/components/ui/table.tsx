4b7788b3c6bc49ab25d20f2fefb49d8f7fcd8a23
Anshor J <anshorje@gmail.com>
2025-05-28 22:15:59 +0700

    Add table component

---
+++ frontend/src/components/ui/table.tsx
@@ -0,0 +1,199 @@
+import {
+  Box,
+  BoxProps,
+  HStack,
+  Table,
+  TableBodyProps,
+  Progress,
+  TableColumnHeaderProps,
+  VStack,
+  Text,
+  Skeleton,
+} from "@chakra-ui/react";
+import { ChevronDown, ChevronUp } from "lucide-react";
+import { useMemo } from "react";
+
+export const TableContainer = (props: BoxProps) => {
+  return (
+    <Box
+      overflowX="auto"
+      maxW="full"
+      overflowY="auto"
+      whiteSpace="nowrap"
+      {...props}
+    />
+  );
+};
+
+interface TableHeaderProps extends TableColumnHeaderProps {
+  enableSort?: boolean;
+  onSort?: (column: string, direction: "asc" | "desc" | null) => void;
+  label?: string;
+  name?: string;
+  sortState?: "asc" | "desc" | null;
+}
+
+export const TableColumnHeader = ({
+  enableSort = false,
+  onSort,
+  label,
+  name = "",
+  children,
+  sortState = null,
+  ...props
+}: TableHeaderProps) => {
+  const getNextSortState = () => {
+    if (sortState === "asc") return "desc";
+    if (sortState === "desc") return null;
+    return "asc";
+  };
+  return (
+    <Table.ColumnHeader
+      cursor={enableSort ? "pointer" : "default"}
+      onClick={() => enableSort && onSort!(name!, getNextSortState())}
+      {...props}
+    >
+      {children}
+
+      {enableSort && (
+        <HStack justifyContent="left">
+          <span>{label}</span>
+          <VStack
+            gap={0}
+            cursor="pointer"
+            onClick={() => onSort!(name!, getNextSortState())}
+          >
+            <Box
+              as={ChevronUp}
+              // @ts-expect-error size is not a valid prop for Box
+              size={12}
+              strokeWidth={3}
+              color={sortState === "asc" ? "gray.800" : "gray.400"}
+              marginBottom={-0.5}
+            />
+            <Box
+              as={ChevronDown}
+              // @ts-expect-error size is not a valid prop for Box
+              size={12}
+              strokeWidth={3}
+              color={sortState === "desc" ? "gray.800" : "gray.400"}
+              marginTop={-0.5}
+            />
+          </VStack>
+          {/* </IconButton> */}
+        </HStack>
+      )}
+
+      {!enableSort && <span>{label}</span>}
+    </Table.ColumnHeader>
+  );
+};
+
+interface CustomTableBodyProps extends TableBodyProps {
+  isFetching?: boolean;
+  isEmpty?: boolean;
+  cols: number;
+}
+
+export const TableBody = ({
+  isFetching = false,
+  isEmpty = false,
+  children,
+  ...props
+}: CustomTableBodyProps) => {
+  return (
+    <Table.Body {...props}>
+      <TableRowProgress isFetching={isFetching} cols={props.cols} />
+      <TableRowEmpty isEmpty={isEmpty} cols={props.cols} />
+      <TableRowSkeleton
+        isEmpty={isEmpty}
+        isFetching={isFetching}
+        cols={props.cols}
+      />
+      {children}
+    </Table.Body>
+  );
+};
+
+const TableRowProgress = ({
+  isFetching,
+  cols = 1,
+}: {
+  isFetching: boolean;
+  cols: number;
+}) => {
+  return (
+    <Table.Row p="0">
+      <Table.Cell colSpan={cols} p="0">
+        {isFetching ? (
+          <Progress.Root size="xs" w="full" value={null}>
+            <Progress.Track bgColor="gray.300">
+              <Progress.Range />
+            </Progress.Track>
+          </Progress.Root>
+        ) : (
+          <Box minH="6px" bgColor="gray.300" />
+        )}
+      </Table.Cell>
+    </Table.Row>
+  );
+};
+
+const TableRowEmpty = ({
+  cols = 1,
+  isEmpty,
+}: {
+  cols: number;
+  isEmpty: boolean;
+}) => {
+  if (!isEmpty) return null;
+  return (
+    <Table.Row p="0">
+      <Table.Cell colSpan={cols}>
+        <Text textAlign="center" py={5} color="gray.500">
+          Tidak ada data
+        </Text>
+      </Table.Cell>
+    </Table.Row>
+  );
+};
+
+const TableRowSkeleton = ({
+  cols = 1,
+  isEmpty,
+  isFetching,
+}: {
+  cols: number;
+  isEmpty: boolean;
+  isFetching: boolean;
+}) => {
+  const columns = useMemo(() => Array.from(Array(cols).keys()), [cols]);
+
+  if (isEmpty && isFetching) {
+    return (
+      <>
+        <Table.Row>
+          {columns.map((i) => (
+            <Table.Cell colSpan={cols} key={i + "a"}>
+              <Skeleton h="20px" w="full" />
+            </Table.Cell>
+          ))}
+        </Table.Row>
+        <Table.Row>
+          {columns.map((i) => (
+            <Table.Cell colSpan={cols} key={i + "b"}>
+              <Skeleton h="20px" w="full" />
+            </Table.Cell>
+          ))}
+        </Table.Row>
+        <Table.Row>
+          {columns.map((i) => (
+            <Table.Cell colSpan={cols} key={i + "c"}>
+              <Skeleton h="20px" w="full" />
+            </Table.Cell>
+          ))}
+        </Table.Row>
+      </>
+    );
+  }
+};
