6d5932229a1136fb38e036bbbd797bf62d66c37c
Anshor J <anshorje@gmail.com>
2025-06-16 11:01:26 +0700

    Add permission and role enum

---
+++ backend/src/constants/permissions.ts
@@ -0,0 +1,5 @@
+export enum Permission {
+  ManageUsers = "action.manage_users",
+  ManageRoles = "action.manage_roles",
+  ManagePermissions = "action.manage_permissions",
+}
---
+++ backend/src/constants/roles.ts
@@ -0,0 +1,7 @@
+export enum Role {
+  Admin = "admin",
+  Principal = "principal",
+  Staff = "staff",
+  Student = "student",
+  Teacher = "teacher",
+}
