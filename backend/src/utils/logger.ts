da4d2225d8de1837b82c93e25bae8b98f336a119
Anshor J <anshorje@gmail.com>
2025-06-21 13:41:10 +0700

    Handle in serverless logger

--- backend/src/utils/logger.ts
+++ backend/src/utils/logger.ts
@@ -2,25 +2,24 @@ import pino from "pino";
 import fs from "fs";
 import path from "path";
 import dayjs from "dayjs";
+import config from "~/config";
 
-//!BUG : LOG TO FILE DOESNWORK IN GOOGLE CLOUD RUN (MUST BE TO /TMP FOLDER)
-// const logDir = path.join(process.cwd(), "logs");
-// if (!fs.existsSync(logDir)) fs.mkdirSync(logDir);
-// const getLogFileName = () =>
-//   path.join(logDir, `${dayjs().format("YYYY-MM-DD")}.log`);
-
-// const logger = pino({ level: "info" }, pino.destination(getLogFileName()));
+const inServerless = config.inServerless;
 
 const logger = pino({
-  level: "info",
-  transport: {
-    target: "pino-pretty",
-    options: {
-      colorize: true,
-      levelFirst: true,
-      translateTime: "yyyy-mm-dd HH:MM:ss",
-    },
-  },
+  level: config.loggerLevel,
+  ...(inServerless
+    ? {} // Use default JSON logging for production
+    : {
+        transport: {
+          target: "pino-pretty",
+          options: {
+            colorize: true,
+            levelFirst: true,
+            translateTime: "yyyy-mm-dd HH:MM:ss",
+          },
+        },
+      }),
 });
 
 export default logger;
