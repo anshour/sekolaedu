828f4c34d86f677a017256729b2ca76b5b053f48
Anshor J <anshorje@gmail.com>
2025-07-16 22:44:18 +0700

    Initiate package shared

---
+++ packages/shared/eslint.config.js
@@ -0,0 +1,18 @@
+// @ts-check
+import eslint from "@eslint/js";
+import tseslint from "typescript-eslint";
+
+export default tseslint.config(
+  {
+    ignores: ["dist/**", "node_modules/**"],
+  },
+  eslint.configs.recommended,
+  ...tseslint.configs.recommended,
+  {
+    languageOptions: {
+      parserOptions: {
+        project: "./tsconfig.json",
+      },
+    },
+  }
+);
