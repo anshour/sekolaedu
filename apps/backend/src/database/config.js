53586548c23d093d21b7ce50e2d67acee2b3e066
Anshor J <anshorje@gmail.com>
2025-08-09 19:47:04 +0700

    Create config sequelize for cli

---
+++ apps/backend/src/database/config.js
@@ -0,0 +1,31 @@
+require("dotenv").config();
+require("tsx");
+
+const dbConfig = {
+  development: {
+    username: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
+    host: process.env.DB_HOST,
+    dialect: "postgres",
+    migrationStorageTableName: "sequelize_meta",
+  },
+  test: {
+    username: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
+    host: process.env.DB_HOST,
+    dialect: "postgres",
+    migrationStorageTableName: "sequelize_meta",
+  },
+  production: {
+    username: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
+    host: process.env.DB_HOST,
+    dialect: "postgres",
+    migrationStorageTableName: "sequelize_meta",
+  },
+};
+
+module.exports = dbConfig;
