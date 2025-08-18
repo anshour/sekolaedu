3ce802601a3eb7594d0c1c4fc83a222dbc496a77
Anshor J <anshorje@gmail.com>
2025-08-18 18:19:52 +0700

    Refactor SubjectService to use Sequelize methods for database operations and improve query structure

--- apps/backend/src/services/subject-service.ts
+++ apps/backend/src/services/subject-service.ts
@@ -1,47 +1,65 @@
-import { paginate } from "~/utils/pagination";
 import { PaginationParams, PaginationResult } from "~/types/pagination";
 import { HttpError } from "~/types/http-error";
 import TeacherService from "./teacher-service";
 import ClassroomService from "./classroom-service";
-import { SubjectAttribute } from "~/models";
+import {
+  ClassroomModel,
+  SubjectAttribute,
+  SubjectModel,
+  TeacherModel,
+  UserModel,
+} from "~/models";
+import buildOrderQuery from "~/utils/query/build-order-query";
+import buildWhereQuery from "~/utils/query/build-where-query";
+import { CreationAttributes, Op } from "sequelize";
 
 class SubjectService {
   static async getAll(
     params: PaginationParams,
   ): Promise<PaginationResult<SubjectAttribute>> {
-    const query = db("subjects")
-      .join("classrooms", "subjects.classroom_id", "classrooms.id")
-      .leftJoin("teachers", "subjects.teacher_id", "teachers.id")
-      .leftJoin("users", "teachers.user_id", "users.id")
-      .select(
-        "subjects.*",
-        "classrooms.name as classroom_name",
-        "users.name as teacher_name",
-      );
+    const whereQuery = buildWhereQuery(params.filter, {
+      classroom_id: (value) => ({ [Op.eq]: value }),
+      academic_year_id: (value) => ({ [Op.eq]: value }),
+      name: (value) => ({ [Op.iLike]: `%${value}%` }),
+    });
 
-    const data = await paginate<SubjectAttribute>(query, params, "subjects.id");
+    const orderQuery = buildOrderQuery(params.sort);
 
-    return data;
+    const subjects = await SubjectModel.paginate({
+      page: params.page,
+      limit: params.limit,
+      where: whereQuery,
+      order: orderQuery,
+      include: [
+        {
+          association: "teacher",
+          include: [
+            {
+              association: "user",
+              attributes: ["id", "name"],
+            },
+          ],
+        },
+        {
+          association: "classroom",
+        },
+      ],
+    });
+
+    return subjects;
   }
 
   static async getById(id: number): Promise<SubjectAttribute | null> {
-    const subject = await db("subjects")
-      .join("classrooms", "subjects.classroom_id", "classrooms.id")
-      .leftJoin("teachers", "subjects.teacher_id", "teachers.id")
-      .leftJoin("users", "teachers.user_id", "users.id")
-      .select(
-        "subjects.*",
-        "classrooms.name as classroom_name",
-        "users.name as teacher_name",
-      )
-      .where("subjects.id", id)
-      .first();
+    const subject = await SubjectModel.scope("withRelations").findOne({
+      where: { id },
+      raw: true,
+    });
 
-    return subject || null;
+    return subject;
   }
 
   static async createSubject(
-    subjectData: Partial<SubjectAttribute>,
+    subjectData: CreationAttributes<SubjectModel>,
     userId: number,
   ): Promise<SubjectAttribute> {
     const classroom = await ClassroomService.getById(subjectData.classroom_id!);
@@ -55,37 +73,22 @@ class SubjectService {
       throw new HttpError("User is not a teacher", 403);
     }
 
-    const existingSubject = await db("subjects")
-      .where({
-        classroom_id: subjectData.classroom_id,
-        name: subjectData.name,
-      })
-      .first();
+    const subject = await SubjectModel.create({
+      ...subjectData,
+      academic_year_id: classroom.academic_year_id,
+      teacher_id: teacher.id,
+    });
 
-    if (existingSubject) {
-      throw new HttpError(
-        "SubjectAttribute with this name already exists in the classroom",
-        400,
-      );
-    }
-
-    const [insertedSubject] = await db("subjects")
-      .insert({
-        ...subjectData,
-        teacher_id: teacher.id,
-      })
-      .returning("id");
-
-    return this.getById(insertedSubject.id) as Promise<SubjectAttribute>;
+    return subject;
   }
 
   static async updateSubject(
     id: number,
-    updateData: Partial<SubjectAttribute>,
+    updateData: CreationAttributes<SubjectModel>,
   ): Promise<SubjectAttribute> {
     const existingSubject = await this.getById(id);
     if (!existingSubject) {
-      throw new HttpError("SubjectAttribute not found", 404);
+      throw new HttpError("Subject with the id not found", 404);
     }
 
     // If updating classroom_id, validate the new classroom exists
@@ -97,60 +100,35 @@ class SubjectService {
       }
     }
 
-    // If updating name or classroom_id, check for duplicates
-    if (updateData.name || updateData.classroom_id) {
-      const checkName = updateData.name || existingSubject.name;
-      const checkClassroomId =
-        updateData.classroom_id || existingSubject.classroom_id;
+    await SubjectModel.update(updateData, { where: { id } });
 
-      const duplicateSubject = await db("subjects")
-        .where({
-          classroom_id: checkClassroomId,
-          name: checkName,
-        })
-        .whereNot("id", id)
-        .first();
-
-      if (duplicateSubject) {
-        throw new HttpError(
-          "Subject with this name already exists in the classroom",
-          400,
-        );
-      }
-    }
-
-    await db("subjects").where({ id }).update(updateData);
     return this.getById(id) as Promise<SubjectAttribute>;
   }
 
   static async deleteSubject(id: number): Promise<void> {
-    const subject = await this.getById(id);
-    if (!subject) {
-      throw new HttpError("Subject not found", 404);
-    }
-
+    //TODO: DELETE SUBJECT RELATIONS
     // Check if subject is being used in other tables (examinations, lessons, etc.)
-    const examinations = await db("examinations")
-      .where({ subject_id: id })
-      .first();
+    // const examinations = await db("examinations")
+    //   .where({ subject_id: id })
+    //   .first();
 
-    if (examinations) {
-      throw new HttpError(
-        "Cannot delete subject. It is being used in examinations",
-        400,
-      );
-    }
+    // if (examinations) {
+    //   throw new HttpError(
+    //     "Cannot delete subject. It is being used in examinations",
+    //     400,
+    //   );
+    // }
 
-    const lessons = await db("lessons").where({ subject_id: id }).first();
+    // const lessons = await db("lessons").where({ subject_id: id }).first();
 
-    if (lessons) {
-      throw new HttpError(
-        "Cannot delete subject. It is being used in lessons",
-        400,
-      );
-    }
+    // if (lessons) {
+    //   throw new HttpError(
+    //     "Cannot delete subject. It is being used in lessons",
+    //     400,
+    //   );
+    // }
 
-    await db("subjects").where({ id }).del();
+    await SubjectModel.destroy({ where: { id } });
   }
 }
 
